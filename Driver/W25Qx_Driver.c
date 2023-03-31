a3287d46d2973f10864ee594320bbdcfcfaf31ff
Kunal Salvi <kunalsalvi63@gmail.com>
2023-03-31 18:56:55 -0500

    W25Qx Driver Updated and Completed

--- Driver/W25Qx_Driver.c
+++ Driver/W25Qx_Driver.c
@@ -8,8 +8,10 @@
 
 #include "W25Qx_Driver.h"
 
-SPI_Config W25QX;
+uint32_t Last_Address ;
 
+SPI_Config W25QX;
+DMA_Init_Typedef W25Qx_DMA, W25Qx_DMA_RX;
 
 struct Status_Register
 {
@@ -126,19 +128,23 @@ void W25QX_Init(W25Qx_Typedef *self)
 	W25QX.type = SPI_Full_Duplex_Master;
 	W25QX.mode = SPI_Master;
 	W25QX.LSB_MSB = 0;
-	W25QX.frequency = 0;
+	W25QX.frequency = 3;
 	W25QX.nCSS_Port = GPIOA;
 	W25QX.nCSS_pin = 15;
 	W25QX.pin_remap = self->pin_remap;
 	W25QX.polarity = 1;
 	W25QX.phase = 1;
+	W25QX.DMA_TX = 1;
+	W25QX.DMA_RX = 0;
 	SPI_Init(W25QX);
+
+/*************************************************************************************/
 
 	W25Qx_Release_Power_Down();
 	Delay_us(5);
 
 	SPI_CSS_Low(W25QX);
-	SPI_TRX_Data(W25QX, 0x4B);
+	SPI_TRX_Data(W25QX, 0x9F);
 	self->Manufacturer_ID = SPI_TRX_Data(W25QX, 0xAA);
 	self->Memory_Type = SPI_TRX_Data(W25QX, 0xAA);
 	self->Capacity = SPI_TRX_Data(W25QX, 0xAA);
@@ -163,7 +169,7 @@ void W25QX_Init(W25Qx_Typedef *self)
 	{
 		self->Last_Address = 0x0FFFFF;
 	}
-	if(self->Memory_Type == 0x14)	//W25Q16
+	if(self->Memory_Type == 64)	//W25Q16
 	{
 		self->Last_Address = 0x1FFFFF;
 	}
@@ -181,30 +187,11 @@ void W25Qx_Release_Power_Down(void)
 	SPI_CSS_High(W25QX);
 }
 
-/*
- * #########################################################################################################
- * ################################################# Read & Write ##########################################
- * #########################################################################################################
- */
 
-void W25Qx_Write_Page(uint32_t address, int *data[], int len)
-{
-	Write_Enable();
-	SPI_CSS_Low(W25QX);
-	SPI_TRX_Data(W25QX, 0x02);
-	SPI_TRX_Data(W25QX, (0xFF0000 & address) >> 16);
-	SPI_TRX_Data(W25QX, (0x00FF00 & address) >> 8);
-	SPI_TRX_Data(W25QX, (0x0000FF & address) >> 0);
-	for(int i = 0; i < len; i++)
-	{
-		SPI_TRX_Data(W25QX, data[i]);
-	}
-	SPI_CSS_High(W25QX);
-	Write_Disable();
-}
 
-void W25Qx_Write_Byte(uint32_t address, int data)
+uint32_t W25Qx_Write_Byte(W25Qx_Typedef *self,uint32_t address, int data)
 {
+	uint32_t temp;
 	Write_Enable();
 	SPI_CSS_Low(W25QX);
 	SPI_TRX_Data(W25QX, 0x02);
@@ -214,30 +201,70 @@ void W25Qx_Write_Byte(uint32_t address, int data)
 	SPI_TRX_Data(W25QX, data);
 	SPI_CSS_High(W25QX);
 	Write_Disable();
+	W25Qx_Store_Last_Position(self, address);
+	temp = W25Qx_Read_Last_Write_Poisition(self);
+	return temp;
+
 }
 
-int W25Qx_Read_Page(uint32_t address)
+
+
+int W25Qx_Read_Byte(uint32_t address)
 {
-	int read;
+	int32_t LA = (int32_t)address;
+	int read = 0;
 	SPI_CSS_Low(W25QX);
 	SPI_TRX_Data(W25QX, 0x03);
-	SPI_TRX_Data(W25QX, (0xFF0000 & address) >> 16);
-	SPI_TRX_Data(W25QX, (0x00FF00 & address) >> 8);
-	SPI_TRX_Data(W25QX, (0x0000FF & address) >> 0);
+	SPI_TRX_Data(W25QX, (0xFF0000 & LA) >> 16);
+	SPI_TRX_Data(W25QX, (0x00FF00 & LA) >> 8);
+	SPI_TRX_Data(W25QX, (0x0000FF & LA) >> 0);
 	read = SPI_TRX_Data(W25QX, 0xAA);
 	SPI_CSS_High(W25QX);
 	return read;
 }
 
+
+
+
 void W25Qx_Store_Last_Position(W25Qx_Typedef *self, uint64_t last_address)
 {
 	uint32_t write[3];
-	write[0] = (0xFF0000 & self->Last_Address) >> 16;
-	write[1] = (0x00FF00 & self->Last_Address) >> 8;
-	write[2] = (0x0000FF & self->Last_Address) >> 0;
-	W25Qx_Write_Byte(self->Last_Address-0, write[0]);
-	W25Qx_Write_Byte(self->Last_Address-1, write[1]);
-	W25Qx_Write_Byte(self->Last_Address-2, write[2]);
+
+	last_address = last_address + 0;
+
+	write[0] = (0xFF0000 & last_address) >> 16;
+	write[1] = (0x00FF00 & last_address) >> 8;
+	write[2] = (0x0000FF & last_address) >> 0;
+
+	Write_Enable();
+	SPI_CSS_Low(W25QX);
+	SPI_TRX_Data(W25QX, 0x02);
+	SPI_TRX_Data(W25QX, (0xFF0000 & (self->Last_Address -0)) >> 16);
+	SPI_TRX_Data(W25QX, (0x00FF00 & (self->Last_Address -0)) >> 8);
+	SPI_TRX_Data(W25QX, (0x0000FF & (self->Last_Address -0)) >> 0);
+	SPI_TRX_Data(W25QX, write[0]);
+	SPI_CSS_High(W25QX);
+	Write_Disable();
+
+	Write_Enable();
+	SPI_CSS_Low(W25QX);
+	SPI_TRX_Data(W25QX, 0x02);
+	SPI_TRX_Data(W25QX, (0xFF0000 & (self->Last_Address -1)) >> 16);
+	SPI_TRX_Data(W25QX, (0x00FF00 & (self->Last_Address -1)) >> 8);
+	SPI_TRX_Data(W25QX, (0x0000FF & (self->Last_Address -1)) >> 0);
+	SPI_TRX_Data(W25QX, write[1]);
+	SPI_CSS_High(W25QX);
+	Write_Disable();
+
+	Write_Enable();
+	SPI_CSS_Low(W25QX);
+	SPI_TRX_Data(W25QX, 0x02);
+	SPI_TRX_Data(W25QX, (0xFF0000 & (self->Last_Address -2)) >> 16);
+	SPI_TRX_Data(W25QX, (0x00FF00 & (self->Last_Address -2)) >> 8);
+	SPI_TRX_Data(W25QX, (0x0000FF & (self->Last_Address -2)) >> 0);
+	SPI_TRX_Data(W25QX, write[2]);
+	SPI_CSS_High(W25QX);
+	Write_Disable();
 
 }
 
@@ -245,12 +272,233 @@ uint32_t W25Qx_Read_Last_Write_Poisition(W25Qx_Typedef *self)
 {
 	uint32_t ret;
 	uint32_t read[3];
-	read[0] = W25Qx_Read_Page(self->Last_Address-0);
-	read[1] = W25Qx_Read_Page(self->Last_Address-1);
-	read[2] = W25Qx_Read_Page(self->Last_Address-2);
+	read[0] = W25Qx_Read_Byte(self->Last_Address-0);
+	read[1] = W25Qx_Read_Byte(self->Last_Address-1);
+	read[2] = W25Qx_Read_Byte(self->Last_Address-2);
 	ret =  (read[0] << 16) |  (read[1] << 8) | (read[2] << 0);
 	return ret;
 }
+
+
+void W25Qx_Write_Page(W25Qx_Typedef *self,int32_t address, uint8_t *data, int len)
+{
+	uint32_t LA;
+	uint8_t buffer[300];
+
+	if(address == -1)
+	{
+		LA = W25Qx_Read_Last_Write_Poisition(self);
+	}
+	else
+	{
+		LA = (uint32_t)address;
+	}
+
+	buffer[0] = 0x02;
+	buffer[1] = ((0xFF0000 & LA) >> 16);
+	buffer[2] = ((0x00FF00 & LA) >> 8);
+	buffer[3] = ((0x0000FF & LA) >> 0);
+	DMA_Memory_To_Memory_Transfer(data, 8, 8, (uint32_t)&buffer[4], 1, 1, len);
+
+	Write_Enable();
+
+	W25QX.DMA_TX = 1;
+	SPI_Enable_DMA(W25QX);
+	W25Qx_DMA.DMA_Port = SPI1_DMA_Port;
+	W25Qx_DMA.Stream = SPI1_DMA_TX_STREAM5;
+	W25Qx_DMA.channel = SPI1_DMA_TX_CHANNEL;
+	W25Qx_DMA.Transfer_Complete_interrupt = 1;
+	W25Qx_DMA.circular_buff_enable = 0;
+	W25Qx_DMA.memory_data_size = Byte;
+	W25Qx_DMA.periphera_data_size = Byte;
+	W25Qx_DMA.direction = DMA_Direction_Memory_to_Peripheral;
+	DMA_Init(&W25Qx_DMA);
+
+	W25Qx_DMA.Source_Address = (uint32_t)&(self->SPI_Port->DR);
+	W25Qx_DMA.Desitnation_Address = (uint32_t)&(buffer[0]);
+	W25Qx_DMA.data_length = len+4;
+	SPI_CSS_Low(W25QX);
+	DMA_Trigger(&W25Qx_DMA,1);
+	Delay_us(5);
+	SPI_CSS_High(W25QX);
+	SPI_Disable_DMA(W25QX);
+	DMA_DeInit(&W25Qx_DMA);
+	Write_Disable();
+	LA = LA + len;
+	W25Qx_Store_Last_Position(self, LA);
+//	DMA_DeInit(&W25Qx_DMA);
+
+}
+
+void W25Qx_Read_Page(W25Qx_Typedef *self,int32_t starting_address, int32_t ending_address, uint8_t *data, uint32_t length)
+{
+	W25QX.DMA_TX = 1;
+	W25QX.DMA_RX = 1;
+	SPI_Enable_DMA(W25QX);
+
+	uint32_t source_dummy = 0xAA;
+	uint8_t tx_buffer[300];
+
+
+	uint32_t ret_value;
+
+	if(ending_address == -1)
+	{
+		ending_address = starting_address + length;
+		ret_value = ending_address;
+	}
+	if(length == -1)
+	{
+		length = ending_address - starting_address;
+		ret_value = length;
+	}
+
+	tx_buffer[0] = 0x03;
+	tx_buffer[1] = ((0xFF0000 & starting_address) >> 16);
+	tx_buffer[2] = ((0x00FF00 & starting_address) >> 8);
+	tx_buffer[3] = ((0x0000FF & starting_address) >> 0);
+	DMA_Memory_To_Memory_Transfer(&source_dummy, 8, 8, (uint32_t)&tx_buffer[4], 0, 1, length);
+
+
+	W25Qx_DMA.DMA_Port = SPI1_DMA_Port;
+	W25Qx_DMA.Stream = SPI1_DMA_TX_STREAM5;
+	W25Qx_DMA.channel = SPI1_DMA_TX_CHANNEL;
+	W25Qx_DMA.Transfer_Complete_interrupt = 1;
+	W25Qx_DMA.circular_buff_enable = 0;
+	W25Qx_DMA.memory_data_size = Byte;
+	W25Qx_DMA.periphera_data_size = Byte;
+	W25Qx_DMA.direction = DMA_Direction_Memory_to_Peripheral;
+	DMA_Init(&W25Qx_DMA);
+
+	W25Qx_DMA.Source_Address = (uint32_t)&(self->SPI_Port->DR);
+	W25Qx_DMA.Desitnation_Address = (uint32_t)&(tx_buffer[0]);
+	W25Qx_DMA.data_length = length+5;
+
+	DMA_Set_Target(&W25Qx_DMA);
+
+	W25Qx_DMA_RX.DMA_Port = SPI1_DMA_Port;
+	W25Qx_DMA_RX.Stream = SPI1_DMA_RX_STREAM2;
+	W25Qx_DMA_RX.channel = SPI1_DMA_RX_CHANNEL;
+	W25Qx_DMA_RX.Transfer_Complete_interrupt = 1;
+	W25Qx_DMA_RX.circular_buff_enable = 0;
+	W25Qx_DMA_RX.memory_data_size = Half_Word;
+	W25Qx_DMA_RX.periphera_data_size = Half_Word;
+	W25Qx_DMA_RX.direction = DMA_Direction_Peripheral_to_Memory;
+	DMA_Init(&W25Qx_DMA_RX);
+
+	W25Qx_DMA_RX.Source_Address = (uint32_t)&(self->SPI_Port->DR);
+	W25Qx_DMA_RX.Desitnation_Address = (uint32_t)&(tx_buffer[0]);
+	W25Qx_DMA_RX.data_length = length+5;
+	DMA_Set_Target(&W25Qx_DMA_RX);
+
+
+	SPI_CSS_Low(W25QX);
+//	Delay_us(10);
+	DMA_Enable(&W25Qx_DMA);
+	DMA_Enable(&W25Qx_DMA_RX);
+
+	while((DMA2 -> LISR & (DMA_LISR_TCIF2_Msk)) == 0){}
+	DMA2 -> LIFCR |= DMA_LIFCR_CTCIF2;
+	DMA2 -> LIFCR |= DMA_LIFCR_CHTIF2;
+
+	while((DMA2 -> HISR & (DMA_HISR_TCIF5_Msk)) == 0){}
+	DMA2 -> HIFCR |= DMA_HIFCR_CTCIF5;
+	DMA2 -> HIFCR |= DMA_HIFCR_CHTIF5;
+
+	SPI_CSS_High(W25QX);
+	DMA_Disable(&W25Qx_DMA);
+	DMA_Disable(&W25Qx_DMA_RX);
+
+	DMA_Memory_To_Memory_Transfer(&tx_buffer[5], 8, 8, data, 1, 1, length);
+
+	return ret_value;
+}
+
+void W25Qx_Read_Page(W25Qx_Typedef *self,int32_t starting_address, int32_t ending_address, uint8_t *data, uint32_t length)
+{
+	W25QX.DMA_TX = 1;
+	W25QX.DMA_RX = 1;
+	SPI_Enable_DMA(W25QX);
+
+	uint32_t source_dummy = 0xAA;
+	uint8_t tx_buffer[300];
+
+
+	uint32_t ret_value;
+
+	if(ending_address == -1)
+	{
+		ending_address = starting_address + length;
+		ret_value = ending_address;
+	}
+	if(length == -1)
+	{
+		length = ending_address - starting_address;
+		ret_value = length;
+	}
+
+	tx_buffer[0] = 0x0B;
+	tx_buffer[1] = ((0xFF0000 & starting_address) >> 16);
+	tx_buffer[2] = ((0x00FF00 & starting_address) >> 8);
+	tx_buffer[3] = ((0x0000FF & starting_address) >> 0);
+	tx_buffer[4] = 0xAA;
+	DMA_Memory_To_Memory_Transfer(&source_dummy, 8, 8, (uint32_t)&tx_buffer[5], 0, 1, length);
+
+
+	W25Qx_DMA.DMA_Port = SPI1_DMA_Port;
+	W25Qx_DMA.Stream = SPI1_DMA_TX_STREAM5;
+	W25Qx_DMA.channel = SPI1_DMA_TX_CHANNEL;
+	W25Qx_DMA.Transfer_Complete_interrupt = 1;
+	W25Qx_DMA.circular_buff_enable = 0;
+	W25Qx_DMA.memory_data_size = Byte;
+	W25Qx_DMA.periphera_data_size = Byte;
+	W25Qx_DMA.direction = DMA_Direction_Memory_to_Peripheral;
+	DMA_Init(&W25Qx_DMA);
+
+	W25Qx_DMA.Source_Address = (uint32_t)&(self->SPI_Port->DR);
+	W25Qx_DMA.Desitnation_Address = (uint32_t)&(tx_buffer[0]);
+	W25Qx_DMA.data_length = length+6;
+
+	DMA_Set_Target(&W25Qx_DMA);
+
+	W25Qx_DMA_RX.DMA_Port = SPI1_DMA_Port;
+	W25Qx_DMA_RX.Stream = SPI1_DMA_RX_STREAM2;
+	W25Qx_DMA_RX.channel = SPI1_DMA_RX_CHANNEL;
+	W25Qx_DMA_RX.Transfer_Complete_interrupt = 1;
+	W25Qx_DMA_RX.circular_buff_enable = 0;
+	W25Qx_DMA_RX.memory_data_size = Half_Word;
+	W25Qx_DMA_RX.periphera_data_size = Half_Word;
+	W25Qx_DMA_RX.direction = DMA_Direction_Peripheral_to_Memory;
+	DMA_Init(&W25Qx_DMA_RX);
+
+	W25Qx_DMA_RX.Source_Address = (uint32_t)&(self->SPI_Port->DR);
+	W25Qx_DMA_RX.Desitnation_Address = (uint32_t)&(tx_buffer[0]);
+	W25Qx_DMA_RX.data_length = length+6;
+	DMA_Set_Target(&W25Qx_DMA_RX);
+
+
+	SPI_CSS_Low(W25QX);
+//	Delay_us(10);
+	DMA_Enable(&W25Qx_DMA);
+	DMA_Enable(&W25Qx_DMA_RX);
+
+	while((DMA2 -> LISR & (DMA_LISR_TCIF2_Msk)) == 0){}
+	DMA2 -> LIFCR |= DMA_LIFCR_CTCIF2;
+	DMA2 -> LIFCR |= DMA_LIFCR_CHTIF2;
+
+	while((DMA2 -> HISR & (DMA_HISR_TCIF5_Msk)) == 0){}
+	DMA2 -> HIFCR |= DMA_HIFCR_CTCIF5;
+	DMA2 -> HIFCR |= DMA_HIFCR_CHTIF5;
+
+	SPI_CSS_High(W25QX);
+	DMA_Disable(&W25Qx_DMA);
+	DMA_Disable(&W25Qx_DMA_RX);
+
+	DMA_Memory_To_Memory_Transfer(&tx_buffer[6], 8, 8, data, 1, 1, length);
+
+	return ret_value;
+}
+
 
 /*
  * #########################################################################################################
@@ -271,12 +519,11 @@ int W25Qx_Chip_Erase(void)
 	return 1;
 }
 
-int W25Qx_Block_Erase_4KB(uint32_t address)
+int W25Qx_Block_Erase(enum Blocks Block, uint32_t address)
 {
-
 	Write_Enable();
 	SPI_CSS_Low(W25QX);
-	SPI_TRX_Data(W25QX, 0x20);
+	SPI_TRX_Data(W25QX, Block);
 	SPI_TRX_Data(W25QX, (0xFF0000 & address) >> 16);
 	SPI_TRX_Data(W25QX, (0x00FF00 & address) >> 8);
 	SPI_TRX_Data(W25QX, (0x0000FF & address) >> 0);
@@ -286,40 +533,14 @@ int W25Qx_Block_Erase_4KB(uint32_t address)
 	}while(SR.BUSY == 1);
 	Write_Disable();
 	return 1;
-}
 
-int W25Qx_Block_Erase_32KB(uint32_t address)
-{
-
-	Write_Enable();
-	SPI_CSS_Low(W25QX);
-	SPI_TRX_Data(W25QX, 0x53);
-	SPI_TRX_Data(W25QX, (0xFF0000 & address) >> 16);
-	SPI_TRX_Data(W25QX, (0x00FF00 & address) >> 8);
-	SPI_TRX_Data(W25QX, (0x0000FF & address) >> 0);
-	SPI_CSS_High(W25QX);
-	do{
-		Read_Status_Register();
-	}while(SR.BUSY == 1);
-	Write_Disable();
-	return 1;
 }
 
 
 
-int W25Qx_Block_Erase_64KB(uint32_t address)
-{
 
-	Write_Enable();
-	SPI_CSS_Low(W25QX);
-	SPI_TRX_Data(W25QX, 0xD8);
-	SPI_TRX_Data(W25QX, (0xFF0000 & address) >> 16);
-	SPI_TRX_Data(W25QX, (0x00FF00 & address) >> 8);
-	SPI_TRX_Data(W25QX, (0x0000FF & address) >> 0);
-	SPI_CSS_High(W25QX);
-	do{
-		Read_Status_Register();
-	}while(SR.BUSY == 1);
-	Write_Disable();
-	return 1;
-}
+
+/*$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ NOT TESTED FUNCTIONS $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$*/
+
+
+
--- Driver/W25Qx_Driver.h
+++ Driver/W25Qx_Driver.h
@@ -12,7 +12,7 @@
 #include "SPI.h"
 #include "Console.h"
 
-
+#define NULL_ADDRESS -1
 
 
 typedef struct W25Qx_Typedef{
@@ -25,19 +25,156 @@ typedef struct W25Qx_Typedef{
 	uint64_t Last_Address;
 }W25Qx_Typedef;
 
+enum Blocks
+{
+	Block_4KB  = 0x20,
+	Block_32KB = 0x52,
+	Block_64KB = 0xD8,
+};
 
+/*$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ TESTED FUNCTIONS $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$*/
 
+/***************************************************************************************
+ * @name:		W25QX_Init
+ * @brief:		Initializes the W25Qx flash chip. Clock Speed is at highest
+ * @in_param:	.SPI_Port = SPI1
+ * 				.pin_remap = true
+ * @out_param:	.Manufacturer_ID
+ * 				.Memory_Type
+ * 				.Capacity
+ * 				.Unique_ID
+ * 				.Last_Address (Not used in main program)
+ * @tests:		Performed all functions without failing
+ */
 void W25QX_Init(W25Qx_Typedef *self);
+
+/***************************************************************************************
+ * @name:		W25Qx_Release_Power_Down
+ * @brief:		Releases W25Qx from power down mode.
+ * @in_param:	none
+ * @out_param:	none
+ * @tests:		Performed all functions without failing
+ */
 void W25Qx_Release_Power_Down(void);
-void W25Qx_Write_Page(uint32_t address, int *data[], int len);
-void W25Qx_Write_Byte(uint32_t address, int data);
-int W25Qx_Read_Page(uint32_t address);
+
+/***************************************************************************************
+ * @name:		W25QX_Init
+ * @brief:		Writes a byte to the memory at a particular location
+ * @in_param:	W25Qx_Typedef -> structure
+ * 				address       -> address of the data to be stored
+ * 				data		  -> byte to be stored
+ * @out_param:	last_address of the data
+ * @tests:		Performed all functions without failing
+ */
+uint32_t W25Qx_Write_Byte(W25Qx_Typedef *self,uint32_t address, int data);
+
+/***************************************************************************************
+ * @name:		W25Qx_Store_Last_Position
+ * @brief:		Stores the last memory address of the previous data at the end of the
+ * 				chip memory
+ * @in_param:	W25Qx_Typedef -> structure
+ * 				address       -> address of the data to be stored
+ * @out_param:	none
+ * @tests:		Performed all functions without failing
+ */
 void W25Qx_Store_Last_Position(W25Qx_Typedef *self, uint64_t last_address);
+
+/***************************************************************************************
+ * @name:		W25Qx_Read_Last_Write_Poisition
+ * @brief:		Reads the last memory address of the previous data which is store at
+ * 				the end of the chip memory
+ * @in_param:	W25Qx_Typedef -> structure
+ * @out_param:	address of the last stored data
+ * @tests:		Performed all functions without failing
+ */
 uint32_t W25Qx_Read_Last_Write_Poisition(W25Qx_Typedef *self);
+
+/***************************************************************************************
+ * @name:		W25Qx_Chip_Erase
+ * @brief:		Erases the entire chip
+ * @in_param:	none
+ * @out_param:	none
+ * @tests:		Performed all functions without failing
+ */
 int W25Qx_Chip_Erase(void);
-int W25Qx_Block_Erase_4KB(uint32_t address);
-int W25Qx_Block_Erase_32KB(uint32_t address);
-int W25Qx_Block_Erase_64KB(uint32_t address);
+
+/***************************************************************************************
+ * @name:		W25Qx_Block_Erase
+ * @brief:		Erases a certain block of the chip that can be selected by passing a
+ * 				suitable value given in the enum Blocks.
+ * @in_param:	enum Blocks -> Block
+ * 				address     -> starting address of the block to be erased
+ * @out_param:	1
+ * @tests:		Performed all functions without failing
+ */
+int W25Qx_Block_Erase(enum Blocks Block, uint32_t address);
+
+/***************************************************************************************
+ * @name:		W25Qx_Read_Byte
+ * @brief:		Reads one byte pointed by the memory
+ * @in_param:	address     -> particular address of the byte to be read
+ * @out_param:	1
+ * @tests:		Performed all functions without failing
+ */
+int W25Qx_Read_Byte(uint32_t address);
+
+/**************************************************************************************
+ * @name:		W25Qx_Write_Page
+ * @brief:		Writes a chunk of data to the memory pointed at an address. To write a
+ * 				block of data, call this function and pass the instance of the chip
+ * 				created earlier, the starting address of the block or if the starting
+ * 				address is not known, pass -1 so that the last address can be used as
+ * 				a starting address, pointer to the data block and the length of the
+ * 				data.
+ * @in_param:	W25Qx_Typedef -> structure
+ * 				address       -> Starting address for the block or -1
+ * 				data[]		  -> pointer of the data to be sent to the chip
+ * 				length		  -> size of the data to be sent
+ * @out_param:	none
+ * @tests:		Not yet tested.
+ */
+void W25Qx_Write_Page(W25Qx_Typedef *self,int32_t address, uint8_t *data, int len);
+
+/**************************************************************************************
+ * @name:		W25Qx_Read_Page
+ * @brief:		Reads a block of data from the memory starting from the starting
+ * 				address. The user has to have either the ending address or the length
+ * 				of the block they wish to access. The absence of the parameter should
+ * 				be replaced with -1.
+ * @in_param:	W25Qx_Typedef		-> structure
+ * 				starting_address	-> Starting address for the block
+ * 				ending_address		-> Ending address for the block / -1
+ * 				data				-> pointer of the data to be sent to the chip
+ * 				length				-> length of the block / -1
+ * @out_param:	none
+ * @tests:		Not yet tested.
+ */
+void W25Qx_Read_Page(W25Qx_Typedef *self,int32_t starting_address, int32_t ending_address, uint8_t *data, uint32_t length);
+
+/**************************************************************************************
+ * @name:		W25Qx_Fast_Read_Page
+ * @brief:		Reads fast a block of data from the memory starting from the starting
+ * 				address. The user has to have either the ending address or the length
+ * 				of the block they wish to access. The absence of the parameter should
+ * 				be replaced with -1.
+ * 				Difference between W25Qx_Read_Page and this function is the clock
+ * 				frequency of reading data.
+ * @in_param:	W25Qx_Typedef		-> structure
+ * 				starting_address	-> Starting address for the block
+ * 				ending_address		-> Ending address for the block / -1
+ * 				data				-> pointer of the data to be sent to the chip
+ * 				length				-> length of the block / -1
+ * @out_param:	none
+ * @tests:		Not yet tested.
+ *
+ */
+void W25Qx_Fast_Read_Page(W25Qx_Typedef *self,int32_t starting_address, int32_t ending_address, uint8_t *data, uint32_t length);
+
+
+
+
+
+/*$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ NOT TESTED FUNCTIONS $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$*/
 
 
 #endif /* W25QX_DRIVER_H_ */
